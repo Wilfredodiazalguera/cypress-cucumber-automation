e785358c63db749444ded9e1f1b06d9aa4df4d1d
Will Diaz <wilfredodiazalguera@gmail.com>
2022-07-28 13:37:43 -0600

    Disable videos cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,18 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
-  retries: {
-    openMode: 2,
-  },
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    specPattern: 'cypress/e2e/**/*.feature',
-    baseUrl: 'https://www.saucedemo.com/',
-  },
-})
+   chromeWebSecurity: false,
+   videoCompression: false,
+   retries: {
+      openMode: 2,
+   },
+   e2e: {
+      // We've imported your old cypress plugins here.
+      // You may want to clean this up later by importing these.
+      setupNodeEvents(on, config) {
+         return require("./cypress/plugins/index.js")(on, config);
+      },
+      specPattern: "cypress/e2e/**/*.feature",
+      baseUrl: "https://www.saucedemo.com/",
+   },
+});
