b7c2bd8861ffea087591b7ff58b7dc2fb9b826c4
Will Diaz <wilfredodiazalguera@gmail.com>
2022-07-28 14:05:53 -0600

    Update Cucumber report file

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,15 +1,15 @@
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
-   jsonDir: "cypress/cucumber-json", // ** Path of .json file **//
+   jsonDir: "./cucumber-json", // ** Path of .json file **//
    reportPath: "./reports",
    metadata: {
       browser: {
          name: "Electron 94",
          version: "Electron 94",
       },
-      device: "Windows and Linux",
+      device: "Windows",
       platform: {
-         name: "ubuntu and windows",
+         name: "Wwindows",
          version: "latest",
       },
    },
