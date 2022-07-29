8eeabcd961424853793fd4cacca4c9f102e42687
Will Diaz <wilfredodiazalguera@gmail.com>
2022-07-29 14:43:07 -0600

    Adding new dep to fix cucumber report JSON.log

--- cypress.config.js
+++ cypress.config.js
@@ -7,11 +7,22 @@ module.exports = defineConfig({
       openMode: 2,
    },
    e2e: {
-      // We've imported your old cypress plugins here.
-      // You may want to clean this up later by importing these.
+      //Cucumber preprocessor set-up
+      async setupNodeEvents(on, config) {
+         const bundler = createBundler({
+            plugins: [createEsbuildPlugin(config)],
+         });
+
+         on("file:preprocessor", bundler);
+         await addCucumberPreprocessorPlugin(on, config);
+
+         return config;
+      },
+
       setupNodeEvents(on, config) {
          return require("./cypress/plugins/index.js")(on, config);
       },
+
       specPattern: "cypress/e2e/**/*.feature",
       baseUrl: "https://www.saucedemo.com/",
    },
